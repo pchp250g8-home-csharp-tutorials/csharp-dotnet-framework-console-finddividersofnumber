6ffc25f55f40958c55253f580417f7ab1c16d8ef
Alexander <pchp250g8@tutamail.com>
2026-03-28 12:39:37 +0300

    Fixed Source Code

--- FindDividersOfNumber/Program.cs
+++ FindDividersOfNumber/Program.cs
@@ -12,7 +12,7 @@ namespace FindDividersOfNumber
         {
             Console.WriteLine("Input an integer positive number");
             UInt64.TryParse(Console.ReadLine(), out ulong ulNumber);
-            Console.WriteLine("The number {0} has the folowing dividers:");
+            Console.WriteLine("The number {0} has the folowing divisors:");
             for (ulong i = 1; i <= ulNumber; i++)
             {
                 if (ulNumber % i == 0)
